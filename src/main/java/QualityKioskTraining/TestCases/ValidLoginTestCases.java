34722a628cd12bf91b5eb55c2ec0ef5c5cb280cb
KajalShele <kajalshele@gmail.com>
2021-05-30 20:49:31 +0530

    Orange HRM framework

---
+++ src/main/java/QualityKioskTraining/TestCases/ValidLoginTestCases.java
@@ -0,0 +1,31 @@
+package QualityKioskTraining.TestCases;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import QualityKioskTraining.Pages.LoginPage;
+
+public class ValidLoginTestCases {
+	
+	LoginPage loginPage;
+	@BeforeClass
+	@Parameters("browser")
+	public void init(String browser) {
+		loginPage=new LoginPage(browser);
+	}
+	@Test(priority=1)
+	public void testLoginWithValidCredential() {
+		/*
+		 * open browser
+		 * open url
+		 * enter username
+		 * enter pwd
+		 * click signIn
+		 * verify welcome sreen appears
+		 */
+		loginPage.doLogin("Admin", "admin123");		
+	}
+	
+	
+}
